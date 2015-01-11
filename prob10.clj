7b8456f5d58c518357575e12fc1945462cc9f8ff
tahmidsadik112 <tahmidsadik112@gmail.com>
2015-01-12 04:18:04 +0600

    Summation of primes below 2 million

---
+++ prob10.clj
@@ -0,0 +1,14 @@
+(ns user
+  (require [clojure.set :refer :all]))
+
+(defn square [n]
+  (* n n))
+
+(defn lazy-primes [max]
+  (let [ next-sieve (fn [sieve candidate]
+                     (if-let [x (contains? sieve candidate)]
+                       (difference sieve (range (square candidate) max candidate))
+                       sieve))]
+    (conj (reduce next-sieve (set (range 3 max 2)) (range 3 (Math/ceil (Math/sqrt max)) 2)) 2)))
+
+(time (apply + (lazy-primes 2000000)))
